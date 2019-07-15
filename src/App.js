f6e9b89338297b29b553438b8e3434b97ead618d
eclipse_user <eclipse_user@atguigu.com>
2019-07-15 14:53:08 -0500

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,18 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import {createStore} from 'redux'
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
+        <h1 className="App-title">Welcome to react</h1>
+      </header>
+        <p className="App-intro">
           Edit <code>src/App.js</code> and save to reload.
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
     </div>
   );
 }
